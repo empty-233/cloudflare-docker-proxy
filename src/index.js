dfff8a1c1bad7e1aace432a97cd1d3eddfe26609
kongwu <31300706+empty-233@users.noreply.github.com>
2024-07-24 20:36:24 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.empty233.top": dockerHub,
+  "quay.empty233.top": "https://quay.io",
+  "gcr.empty233.top": "https://gcr.io",
+  "k8s-gcr.empty233.top": "https://k8s.gcr.io",
+  "k8s.empty233.top": "https://registry.k8s.io",
+  "ghcr.empty233.top": "https://ghcr.io",
+  "cloudsmith.empty233.top": "https://docker.cloudsmith.io",
+  "ecr.empty233.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.empty233.top": dockerHub,
 };
 
 function routeByHosts(host) {
